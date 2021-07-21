2ed4eee1f8f4ee5ee597807e8307ee0de5f4aa1f
Nidhi Singh <38832341+NidhiSingh0068@users.noreply.github.com>
2021-07-21 22:03:41 +0530

    Update data_backup_code.py

--- data_backup_code.py
+++ data_backup_code.py
@@ -55,62 +55,39 @@ if __name__ == "__main__":
     
     current_date = datetime.strptime('2020-9-18', '%Y-%m-%d')   #Taking a date as current date
     
-    sat_file_count_for_bucket1 = 0                              #counting for last 4 saturdays in bucket 1
-    sat_file_count_for_bucket2 = 0                              #counting for last 4 saturdays in bucket 1
-      
-    bucket_1 = 'bucket1/'
-    bucket_2 = 'bucket2/'
+                                                                
+    print("Enter bucket name like bucket1 bucket2 (space seperated)")                          
+    bucket = [x for x in input().split()]
 
-    print('Iterating through Bucket 1 directory...\n')
-    print("Deleting following files in bucket 1 : \n")
-    
-    for entry in os.listdir(bucket_1):
-        start = entry.rfind('_') + 1
-        end = entry.find('.')
-        f_date = entry[start:end]
+    for i in range(len(bucket)):
+        sat_file_count_for_bucket = 0
+        print('Iterating through  ' +bucket[i] +' directory...\n')
+        print("Deleting following files in " +bucket[i]+" : \n")
+        
+        for entry in os.listdir(bucket[i]+'/'):
+            start = entry.rfind('_') + 1
+            end = entry.find('.')
+            f_date = entry[start:end]
 
-        #datetime of file
-        file_date = datetime.strptime(f_date, '%Y-%m-%d')
-        a = DataBackup(current_date, file_date)
-        if a.check_last_Fivedays(entry):
-            if a.check_Sat(entry) == False:
-                if a.check_last_Day_of_Month(entry) == False:
-                    print(entry)
-                    os.remove(bucket_1+entry)
-            else:
-                if sat_file_count_for_bucket1 > 4:
+            #datetime of file
+            file_date = datetime.strptime(f_date, '%Y-%m-%d')
+            a = DataBackup(current_date, file_date)
+            if a.check_last_Fivedays(entry):
+                if a.check_Sat(entry) == False:
                     if a.check_last_Day_of_Month(entry) == False:
                         print(entry)
-                        os.remove(bucket_1+entry)
+                        os.remove(bucket[i]+'/'+entry)
                 else:
-                    sat_file_count_for_bucket1 += 1
+                    if sat_file_count_for_bucket > 4:
+                        if a.check_last_Day_of_Month(entry) == False:
+                            print(entry)
+                            os.remove(bucket[i]+'/'+entry)
+                    else:
+                        sat_file_count_for_bucket += 1
 
     
-    print('\nIterating through Bucket 2 directory...\n')
-    print("Deleting following files in bucket 2 : \n")
-    for entry in os.listdir(bucket_2):
-        start = entry.rfind('_') + 1
-        end = entry.find('.')
-        f_date = entry[start:end]
-
-        #datetime of file
-        file_date = datetime.strptime(f_date, '%Y-%m-%d')
-        a = DataBackup(current_date, file_date)
-        if a.check_last_Fivedays(entry):
-            if a.check_Sat(entry) == False:
-                if a.check_last_Day_of_Month(entry) == False:
-                    print(entry)
-                    os.remove(bucket_2+entry)
-            else:
-                if sat_file_count_for_bucket2 > 4:
-                    if a.check_last_Day_of_Month(entry) == False:
-                        print(entry)
-                        os.remove(bucket_2+entry)
-                else:
-                    sat_file_count_for_bucket2 += 1
 
 #printing remaining files in buckets
-print("\nRemaining files in bucket 1 : \n")
-print('\n'.join(os.listdir(path='bucket1/')))
-print("\nRemaining files in bucket 2 : \n")
-print('\n'.join(os.listdir(path='bucket2/')))
+for i in range(len(bucket)):
+    print("\nRemaining files in "+bucket[i]+" : \n")
+    print('\n'.join(os.listdir(path=bucket[i]+'/')))
